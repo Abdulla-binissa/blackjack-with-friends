03dbf3883a7108ddd57f8db3ed0c37f04298bc40
Abdulla Binissa <abdulla.binissa.155@my.csun.edu>
2019-11-14 19:01:44 -0800

    Update CharacterSelectPage.js
    Added form to input unlock password.
    And functions for stuff.

--- src/CharacterSelect/CharacterSelectPage.js
+++ src/CharacterSelect/CharacterSelectPage.js
@@ -5,12 +5,39 @@ import './Character.css';
 import Character from "./Character";
 
 class CharacterSelect extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            characters: [],
+            unlockPassword: ' '
+        };
+    }
+
+    //Unlocks Characters depending on password inputted
+    unlockOnClick() {
+    }
+
+    //Changes 'unlockPassword' to inputted value?
+    unlockInputChange(e) {
+        const unlockPassword = e.target.value;
+        this.setState({ unlockPassword });
+    }
 
     render() {
         return (
             <div>
-                <Character name="NAME" />
+                <div>
+                    <Character name="NAME" />
+                    <Character name="NAME" />
+                    <Character name="NAME" />
+                </div>
+                <div>
+                    <form>
+                        <input type="text" name="character unlock" placeholder="???" value={this.state.unlockPassword} onChange={this.unlockInputChange.bind(this)} />
+                    </form>
+                    <button onClick={() => { this.unlockOnClick() }}> UNLOCK </button>
+                </div>
             </div>
         );
     }
