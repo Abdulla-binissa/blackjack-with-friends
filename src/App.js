d045da06844ec5d27ef2407a16c4afe6f38e99dd
Abdulla <abdulla.binissa.176@gmail.com>
2021-01-12 13:44:13 -0800

    Run on GitHub-Pages
    Built to run on gh-pages instead of netlify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
+import { HashRouter as Router, Route, useHistory } from "react-router-dom";
 import './App.css';
 import './Components/Players.js';
 
